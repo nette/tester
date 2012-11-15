cfed31e4951f642446f7e0a86629d5240506fd2e
David Grudl <david@grudl.com>
2012-11-15 15:17:07 +0100

    CodeCoverage: added full command-line interface

--- Tester/coverage-report.php
+++ Tester/coverage-report.php
@@ -10,8 +10,44 @@
 require_once __DIR__ . '/CodeCoverage/ReportGenerator.php';
 
 
-$file = __DIR__ . '/coverage.dat';
-$root = realpath(__DIR__ . '/../../Nette') . DIRECTORY_SEPARATOR;
+echo '
+Code coverage HTML report generator
+-----------------------------------
+';
 
-$converter = new ReportGenerator($file, $root);
-$converter->render();
+$options = (array) getopt('c:s:o:h', array('help'));
+
+if (!$options) { ?>
+Usage:
+	php coverage-report.php [options]
+
+Options:
+	-c <path>  coverage.dat file (default is coverage.dat)
+	-s <path>  directory with source code
+	-o <path>  output file (default is coverage.html)
+
+<?php
+}
+
+
+
+$options += array(
+	'c' => 'coverage.dat',
+	's' => NULL,
+	'o' => 'coverage.html',
+);
+
+try {
+	$generator = new ReportGenerator($options['c'], $options['s']);
+	if ($options['o'] === '-') {
+		$generator->render();
+	} else {
+		echo "Generating report to $options[o]\n";
+		$generator->render($options['o']);
+		echo "Done.\n";
+	}
+
+} catch (Exception $e) {
+	echo "Fatal error: {$e->getMessage()}\n";
+	die(254);
+}
