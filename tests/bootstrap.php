8041a2cca9f7f51c6250ebb0e17859c29cc5d712
David Grudl <david@grudl.com>
2013-07-07 17:14:41 +0200

    tests: added function test()

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,9 @@ require __DIR__ . '/../Tester/bootstrap.php';
 if (extension_loaded('xdebug')) {
 	Tester\CodeCoverage\Collector::start(__DIR__ . '/coverage.dat');
 }
+
+
+function test(\Closure $function)
+{
+	$function();
+}
