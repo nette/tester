31d2ab9179dcc40c1031a0f4f3d00354fdbe6fd0
Filip Procházka <filip@prochazka.su>
2014-01-14 02:32:13 +0100

    TestCase: Fixed backwards compatibility for listing methods

--- Tester/Framework/TestCase.php
+++ Tester/Framework/TestCase.php
@@ -26,7 +26,7 @@ class TestCase
 	 */
 	public function run($method = NULL)
 	{
-		if ($method === NULL && isset($_SERVER['argv'][1])) {
+		if (($method === NULL || $method === self::LIST_METHODS) && isset($_SERVER['argv'][1])) {
 			if ($_SERVER['argv'][1] === self::LIST_METHODS) {
 				echo json_encode(preg_grep(self::METHOD_PATTERN, get_class_methods($this)));
 				return;
