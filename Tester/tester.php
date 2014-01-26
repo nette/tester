c2a91f7fe241a0535f20a7bde39c13ac0c0f4f82
Miloslav Hůla <miloslav.hula@gmail.com>
2014-01-26 21:09:44 +0100

    tester: fit help to standard 80-column terminal

--- Tester/tester.php
+++ Tester/tester.php
@@ -33,20 +33,20 @@ Tester\Environment::setup();
 
 
 $cmd = new Cmd("Usage:
-	tester.php [options] [<test file> | <directory>]...
+    tester.php [options] [<test file> | <directory>]...
 
 Options:
-	-p <path>            Specify PHP executable to run (default: php-cgi).
-	-c <path>            Look for php.ini in directory <path> or use <path> as php.ini.
-	-log <path>          Write log to file <path>.
-	-d <key=value>...    Define INI entry 'key' with value 'val'.
-	-s                   Show information about skipped tests.
-	--tap                Generate Test Anything Protocol.
-	-j <num>             Run <num> jobs in parallel.
-	-w | --watch <path>  Watch directory.
-	--setup <path>       Script for runner setup.
-	--colors [1|0]       Enable or disable colors.
-	-h | --help          This help.
+    -p <path>            Specify PHP executable to run (default: php-cgi).
+    -c <path>            Look for php.ini file (or look in directory) <path>.
+    -log <path>          Write log to file <path>.
+    -d <key=value>...    Define INI entry 'key' with value 'val'.
+    -s                   Show information about skipped tests.
+    --tap                Generate Test Anything Protocol.
+    -j <num>             Run <num> jobs in parallel.
+    -w | --watch <path>  Watch directory.
+    --setup <path>       Script for runner setup.
+    --colors [1|0]       Enable or disable colors.
+    -h | --help          This help.
 
 ", array(
 	'-c' => array(Cmd::REALPATH => TRUE),
