3b4949d0e3060c2cc0670ec5af8bea890a80e1a9
David Grudl <david@grudl.com>
2015-07-01 17:05:29 +0200

    Assert: fixed pattern %[..]% regexp

--- src/Framework/Assert.php
+++ src/Framework/Assert.php
@@ -33,7 +33,7 @@ class Assert
 		'%f%' => '[+-]?\.?\d+\.?\d*(?:[Ee][+-]?\d+)?', // floating point number
 		'%h%' => '[0-9a-fA-F]+',// one or more HEX digits
 		'%ds%' => '[\\\\/]',    // directory separator
-		'%(\[.*\].*)%' => '$1', // range
+		'%(\[.+\][+*?{},\d]*)%' => '$1', // range
 	);
 
 	/** @var callable  function (AssertException $exception) */
