3815b5895fab9c918ae455f3179c72b8ea89a0ce
Jan Kuchař <honza.kuchar@grifart.cz>
2015-08-29 21:26:28 +0200

    HtmlGenerator: Not evaluated sources are now counted into coverage too

--- src/CodeCoverage/Generators/HtmlGenerator.php
+++ src/CodeCoverage/Generators/HtmlGenerator.php
@@ -96,6 +96,8 @@ class HtmlGenerator extends AbstractGenerator
 				$coverage = round($covered * 100 / $total);
 				$this->totalSum += $total;
 				$this->coveredSum += $covered;
+			} else {
+				$this->totalSum += count(file($entry, FILE_SKIP_EMPTY_LINES));
 			}
 
 			$light = $total ? $total < 5 : count(file($entry)) < 50;
