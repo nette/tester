292df780f3dc392d0b2cf067118c8d38bce36010
Jan Tvrdik <jan.tvrdik@gmail.com>
2016-03-22 11:12:54 +0100

    CodeCoverage: allowed manually calling save() to free memory

--- src/CodeCoverage/Collector.php
+++ src/CodeCoverage/Collector.php
@@ -16,6 +16,18 @@ class Collector
 	/** @var resource */
 	private static $file;
 
+	/** @var string */
+	private static $collector;
+
+
+	/**
+	 * @return bool
+	 */
+	public static function isStarted()
+	{
+		return self::$file !== NULL;
+	}
+
 
 	/**
 	 * Starts gathering the information for code coverage.
@@ -24,30 +36,52 @@ class Collector
 	 */
 	public static function start($file)
 	{
-		if (self::$file) {
+		if (self::isStarted()) {
 			throw new \LogicException('Code coverage collector has been already started.');
 		}
 		self::$file = fopen($file, 'c+');
 
 		if (defined('PHPDBG_VERSION') && PHP_VERSION_ID >= 70000) {
 			phpdbg_start_oplog();
-			$collector = 'collectPhpDbg';
+			self::$collector = 'collectPhpDbg';
 
 		} elseif (extension_loaded('xdebug')) {
 			xdebug_start_code_coverage(XDEBUG_CC_UNUSED | XDEBUG_CC_DEAD_CODE);
-			$collector = 'collectXdebug';
+			self::$collector = 'collectXdebug';
 
 		} else {
 			$alternative = PHP_VERSION_ID >= 70000 ? ' or phpdbg SAPI' : '';
 			throw new \Exception("Code coverage functionality requires Xdebug extension$alternative.");
 		}
 
-		register_shutdown_function(function () use ($collector) {
-			register_shutdown_function(function () use ($collector) {
-				list($positive, $negative) = call_user_func([__CLASS__, $collector]);
-				self::save($positive, $negative);
-			});
+		register_shutdown_function(function () {
+			register_shutdown_function([__CLASS__, 'save']);
 		});
+	}
+
+
+	/**
+	 * Saves information about code coverage. Can be called repeatedly to free memory.
+	 * @return void
+	 */
+	public static function save()
+	{
+		list($positive, $negative) = call_user_func([__CLASS__, self::$collector]);
+
+		flock(self::$file, LOCK_EX);
+		fseek(self::$file, 0);
+		$rawContent = stream_get_contents(self::$file);
+		$original = $rawContent ? unserialize($rawContent) : [];
+		$coverage = array_replace_recursive($negative, $original, $positive);
+
+		fseek(self::$file, 0);
+		ftruncate(self::$file, 0);
+		fwrite(self::$file, serialize($coverage));
+		flock(self::$file, LOCK_UN);
+
+		if (self::$collector === 'collectPhpDbg') {
+			phpdbg_start_oplog();
+		}
 	}
 
 
@@ -95,25 +129,6 @@ class Collector
 		}
 
 		return [$positive, $negative];
-	}
-
-
-	/**
-	 * Saves information about code coverage. Do not call directly.
-	 * @return void
-	 */
-	private static function save(array $positive, array $negative)
-	{
-		flock(self::$file, LOCK_EX);
-		fseek(self::$file, 0);
-		$rawContent = stream_get_contents(self::$file);
-		$original = $rawContent ? unserialize($rawContent) : [];
-		$coverage = array_replace_recursive($negative, $original, $positive);
-
-		fseek(self::$file, 0);
-		ftruncate(self::$file, 0);
-		fwrite(self::$file, serialize($coverage));
-		fclose(self::$file);
 	}
 
 }
