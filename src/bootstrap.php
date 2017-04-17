5687a560e72b99536488c3ae4c8b3e410132b08e
Jan Kuchař <honza.kuchar@grifart.cz>
2017-04-17 11:53:41 +0200

    bootstrap: made idempotent

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,16 +4,16 @@
  * Test environment initialization.
  */
 
-require __DIR__ . '/Framework/Helpers.php';
-require __DIR__ . '/Framework/Environment.php';
-require __DIR__ . '/Framework/DataProvider.php';
-require __DIR__ . '/Framework/Assert.php';
-require __DIR__ . '/Framework/AssertException.php';
-require __DIR__ . '/Framework/Dumper.php';
-require __DIR__ . '/Framework/FileMock.php';
-require __DIR__ . '/Framework/TestCase.php';
-require __DIR__ . '/Framework/DomQuery.php';
-require __DIR__ . '/CodeCoverage/Collector.php';
-require __DIR__ . '/Runner/Job.php';
+require_once __DIR__ . '/Framework/Helpers.php';
+require_once __DIR__ . '/Framework/Environment.php';
+require_once __DIR__ . '/Framework/DataProvider.php';
+require_once __DIR__ . '/Framework/Assert.php';
+require_once __DIR__ . '/Framework/AssertException.php';
+require_once __DIR__ . '/Framework/Dumper.php';
+require_once __DIR__ . '/Framework/FileMock.php';
+require_once __DIR__ . '/Framework/TestCase.php';
+require_once __DIR__ . '/Framework/DomQuery.php';
+require_once __DIR__ . '/CodeCoverage/Collector.php';
+require_once __DIR__ . '/Runner/Job.php';
 
 Tester\Environment::setup();
