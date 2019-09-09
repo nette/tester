85aafde7af70d34d04a5f5f67bea08136a1e0aa2
Jan Barášek <janbarasek@gmail.com>
2019-09-09 13:53:12 +0200

    Environment: File can be undefined.

--- src/Framework/Environment.php
+++ src/Framework/Environment.php
@@ -166,8 +166,8 @@ class Environment
 	public static function getTestAnnotations(): array
 	{
 		$trace = debug_backtrace();
-		$file = $trace[count($trace) - 1]['file'];
-		return Helpers::parseDocComment(file_get_contents($file)) + ['file' => $file];
+		$file = $trace[count($trace) - 1]['file'] ?? null;
+		return Helpers::parseDocComment($file ? file_get_contents($file) : '') + ['file' => $file];
 	}
 
 
