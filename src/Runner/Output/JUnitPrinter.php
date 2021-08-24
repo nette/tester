dce47abd47fec442839bcd9524968aa9672dc419
David <david@grudl.com>
2021-08-24 13:37:01 +0200

    support for PHP 8.1

--- src/Runner/Output/JUnitPrinter.php
+++ src/Runner/Output/JUnitPrinter.php
@@ -61,7 +61,7 @@ class JUnitPrinter implements Tester\Runner\OutputHandler
 
 		switch ($test->getResult()) {
 			case Test::FAILED:
-				$this->buffer .= ">\n\t\t\t<failure message=\"" . htmlspecialchars($test->message) . "\"/>\n\t\t</testcase>\n";
+				$this->buffer .= ">\n\t\t\t<failure message=\"" . htmlspecialchars($test->message, ENT_COMPAT | ENT_HTML5) . "\"/>\n\t\t</testcase>\n";
 				break;
 			case Test::SKIPPED:
 				$this->buffer .= ">\n\t\t\t<skipped/>\n\t\t</testcase>\n";
