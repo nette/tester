a2e2ebc2b6c37247abba17f9389c33e6dee293cf
Matěj Kmínek <m@kminet.eu>
2025-11-30 00:53:25 +0100

    Add working with metrics for lines outside class/struct definitions.
    These lines are correctly represented by <line> xml tag, but should be counted amongst the coverage/project/metrics, affecting statements / coveredStatements metrics

--- src/CodeCoverage/Generators/CloverXMLGenerator.php
+++ src/CodeCoverage/Generators/CloverXMLGenerator.php
@@ -18,210 +18,252 @@ use const FILE_SKIP_EMPTY_LINES;
 
 class CloverXMLGenerator extends AbstractGenerator
 {
-	private static array $metricAttributesMap = [
-		'packageCount' => 'packages',
-		'fileCount' => 'files',
-		'linesOfCode' => 'loc',
-		'linesOfNonCommentedCode' => 'ncloc',
-		'classCount' => 'classes',
-		'methodCount' => 'methods',
-		'coveredMethodCount' => 'coveredmethods',
-		'statementCount' => 'statements',
-		'coveredStatementCount' => 'coveredstatements',
-		'elementCount' => 'elements',
-		'coveredElementCount' => 'coveredelements',
-		'conditionalCount' => 'conditionals',
-		'coveredConditionalCount' => 'coveredconditionals',
-	];
+    private static array $metricAttributesMap = [
+        'packageCount' => 'packages',
+        'fileCount' => 'files',
+        'linesOfCode' => 'loc',
+        'linesOfNonCommentedCode' => 'ncloc',
+        'classCount' => 'classes',
+        'methodCount' => 'methods',
+        'coveredMethodCount' => 'coveredmethods',
+        'statementCount' => 'statements',
+        'coveredStatementCount' => 'coveredstatements',
+        'elementCount' => 'elements',
+        'coveredElementCount' => 'coveredelements',
+        'conditionalCount' => 'conditionals',
+        'coveredConditionalCount' => 'coveredconditionals',
+    ];
 
 
-	public function __construct(string $file, array $sources = [])
-	{
-		if (!extension_loaded('dom') || !extension_loaded('tokenizer')) {
-			throw new \LogicException('CloverXML generator requires DOM and Tokenizer extensions to be loaded.');
-		}
+    public function __construct(string $file, array $sources = [])
+    {
+        if (!extension_loaded('dom') || !extension_loaded('tokenizer')) {
+            throw new \LogicException('CloverXML generator requires DOM and Tokenizer extensions to be loaded.');
+        }
 
-		parent::__construct($file, $sources);
-	}
-
-
-	protected function renderSelf(): void
-	{
-		$time = (string) time();
-		$parser = new PhpParser;
-
-		$doc = new DOMDocument;
-		$doc->formatOutput = true;
-
-		$elCoverage = $doc->appendChild($doc->createElement('coverage'));
-		$elCoverage->setAttribute('generated', $time);
-
-		// TODO: @name
-		$elProject = $elCoverage->appendChild($doc->createElement('project'));
-		$elProject->setAttribute('timestamp', $time);
-		$elProjectMetrics = $elProject->appendChild($doc->createElement('metrics'));
-
-		$projectMetrics = (object) [
-			'packageCount' => 0,
-			'fileCount' => 0,
-			'linesOfCode' => 0,
-			'linesOfNonCommentedCode' => 0,
-			'classCount' => 0,
-			'methodCount' => 0,
-			'coveredMethodCount' => 0,
-			'statementCount' => 0,
-			'coveredStatementCount' => 0,
-			'elementCount' => 0,
-			'coveredElementCount' => 0,
-			'conditionalCount' => 0,
-			'coveredConditionalCount' => 0,
-		];
-
-		foreach ($this->getSourceIterator() as $file) {
-			$file = (string) $file;
-
-			$projectMetrics->fileCount++;
-
-			if (empty($this->data[$file])) {
-				$coverageData = null;
-				$this->totalSum += count(file($file, FILE_SKIP_EMPTY_LINES));
-			} else {
-				$coverageData = $this->data[$file];
-			}
-
-			// TODO: split to <package> by namespace?
-			$elFile = $elProject->appendChild($doc->createElement('file'));
-			$elFile->setAttribute('name', $file);
-			$elFileMetrics = $elFile->appendChild($doc->createElement('metrics'));
-
-			try {
-				$code = $parser->parse(file_get_contents($file));
-			} catch (\ParseError $e) {
-				throw new \ParseError($e->getMessage() . ' in file ' . $file);
-			}
-
-			$fileMetrics = (object) [
-				'linesOfCode' => $code->linesOfCode,
-				'linesOfNonCommentedCode' => $code->linesOfCode - $code->linesOfComments,
-				'classCount' => count($code->classes) + count($code->traits),
-				'methodCount' => 0,
-				'coveredMethodCount' => 0,
-				'statementCount' => 0,
-				'coveredStatementCount' => 0,
-				'elementCount' => 0,
-				'coveredElementCount' => 0,
-				'conditionalCount' => 0,
-				'coveredConditionalCount' => 0,
-			];
-
-			foreach (array_merge($code->classes, $code->traits) as $name => $info) { // TODO: interfaces?
-				$elClass = $elFile->appendChild($doc->createElement('class'));
-				if (($tmp = strrpos($name, '\\')) === false) {
-					$elClass->setAttribute('name', $name);
-				} else {
-					$elClass->setAttribute('namespace', substr($name, 0, $tmp));
-					$elClass->setAttribute('name', substr($name, $tmp + 1));
-				}
-
-				$elClassMetrics = $elClass->appendChild($doc->createElement('metrics'));
-				$classMetrics = $this->calculateClassMetrics($info, $coverageData);
-				self::setMetricAttributes($elClassMetrics, $classMetrics);
-				self::appendMetrics($fileMetrics, $classMetrics);
-			}
-
-			self::setMetricAttributes($elFileMetrics, $fileMetrics);
+        parent::__construct($file, $sources);
+    }
 
 
-			foreach ((array) $coverageData as $line => $count) {
-				if ($count === self::LineDead) {
-					continue;
-				}
+    protected function renderSelf(): void
+    {
+        $time = (string) time();
+        $parser = new PhpParser;
 
-				// Line type can be 'method' but Xdebug does not report such lines as executed.
-				$elLine = $elFile->appendChild($doc->createElement('line'));
-				$elLine->setAttribute('num', (string) $line);
-				$elLine->setAttribute('type', 'stmt');
-				$elLine->setAttribute('count', (string) max(0, $count));
+        $doc = new DOMDocument;
+        $doc->formatOutput = true;
 
-				$this->totalSum++;
-				$this->coveredSum += $count > 0 ? 1 : 0;
-			}
+        $elCoverage = $doc->appendChild($doc->createElement('coverage'));
+        $elCoverage->setAttribute('generated', $time);
 
-			self::appendMetrics($projectMetrics, $fileMetrics);
-		}
+        // TODO: @name
+        $elProject = $elCoverage->appendChild($doc->createElement('project'));
+        $elProject->setAttribute('timestamp', $time);
+        $elProjectMetrics = $elProject->appendChild($doc->createElement('metrics'));
 
-		// TODO: What about reported (covered) lines outside of class/trait definition?
-		self::setMetricAttributes($elProjectMetrics, $projectMetrics);
+        $projectMetrics = (object) [
+                'packageCount' => 0,
+                'fileCount' => 0,
+                'linesOfCode' => 0,
+                'linesOfNonCommentedCode' => 0,
+                'classCount' => 0,
+                'methodCount' => 0,
+                'coveredMethodCount' => 0,
+                'statementCount' => 0,
+                'coveredStatementCount' => 0,
+                'elementCount' => 0,
+                'coveredElementCount' => 0,
+                'conditionalCount' => 0,
+                'coveredConditionalCount' => 0,
+        ];
 
-		echo $doc->saveXML();
-	}
+        foreach ($this->getSourceIterator() as $file) {
+            $file = (string) $file;
+
+            $projectMetrics->fileCount++;
+
+            if (empty($this->data[$file])) {
+                $coverageData = null;
+                $this->totalSum += count(file($file, FILE_SKIP_EMPTY_LINES));
+            } else {
+                $coverageData = $this->data[$file];
+            }
+
+            // TODO: split to <package> by namespace?
+            $elFile = $elProject->appendChild($doc->createElement('file'));
+            $elFile->setAttribute('name', $file);
+            $elFileMetrics = $elFile->appendChild($doc->createElement('metrics'));
+
+            try {
+                $code = $parser->parse(file_get_contents($file));
+            } catch (\ParseError $e) {
+                throw new \ParseError($e->getMessage() . ' in file ' . $file);
+            }
+
+            $fileMetrics = (object) [
+                    'linesOfCode' => $code->linesOfCode,
+                    'linesOfNonCommentedCode' => $code->linesOfCode - $code->linesOfComments,
+                    'classCount' => count($code->classes) + count($code->traits),
+                    'methodCount' => 0,
+                    'coveredMethodCount' => 0,
+                    'statementCount' => 0,
+                    'coveredStatementCount' => 0,
+                    'elementCount' => 0,
+                    'coveredElementCount' => 0,
+                    'conditionalCount' => 0,
+                    'coveredConditionalCount' => 0,
+            ];
+            
+            // Prepare metrics for lines outside class/struct definitions
+            $structuralLines = array_fill(1, $code->linesOfCode + 1, true);
+
+            foreach (array_merge($code->classes, $code->traits) as $name => $info) { // TODO: interfaces?
+                $elClass = $elFile->appendChild($doc->createElement('class'));
+                if (($tmp = strrpos($name, '\\')) === false) {
+                    $elClass->setAttribute('name', $name);
+                } else {
+                    $elClass->setAttribute('namespace', substr($name, 0, $tmp));
+                    $elClass->setAttribute('name', substr($name, $tmp + 1));
+                }
+
+                $elClassMetrics = $elClass->appendChild($doc->createElement('metrics'));
+                $classMetrics = $this->calculateClassMetrics($info, $coverageData);
+ 
+                // mark all lines inside iterated class as non-structurals
+                for ($index = $info->start + 1; $index <= $info->end; $index++) { // + 1 to skip function name
+                    unset($structuralLines[$index]);
+                }
+
+                self::setMetricAttributes($elClassMetrics, $classMetrics);
+                self::appendMetrics($fileMetrics, $classMetrics);
+            }
+
+            //plain metrics - procedural style
+            $structMetrics = $this->calculateStructuralMetrics($structuralLines, $coverageData);
+            self::appendMetrics($fileMetrics, $structMetrics);
+
+            self::setMetricAttributes($elFileMetrics, $fileMetrics);
+
+            
+            foreach ((array) $coverageData as $line => $count) {
+                if ($count === self::LineDead) {
+                    continue;
+                }
+
+                // Line type can be 'method' but Xdebug does not report such lines as executed.
+                $elLine = $elFile->appendChild($doc->createElement('line'));
+                $elLine->setAttribute('num', (string) $line);
+                $elLine->setAttribute('type', 'stmt');
+                $elLine->setAttribute('count', (string) max(0, $count));
+
+                $this->totalSum++;
+                $this->coveredSum += $count > 0 ? 1 : 0;
+            }
+
+            self::appendMetrics($projectMetrics, $fileMetrics);
+        }
+
+        self::setMetricAttributes($elProjectMetrics, $projectMetrics);
+
+        echo $doc->saveXML();
+    }
 
 
-	private function calculateClassMetrics(\stdClass $info, ?array $coverageData = null): \stdClass
-	{
-		$stats = (object) [
-			'methodCount' => count($info->methods),
-			'coveredMethodCount' => 0,
-			'statementCount' => 0,
-			'coveredStatementCount' => 0,
-			'conditionalCount' => 0,
-			'coveredConditionalCount' => 0,
-			'elementCount' => null,
-			'coveredElementCount' => null,
-		];
+    private function calculateClassMetrics(\stdClass $info, ?array $coverageData = null): \stdClass
+    {
+        $stats = (object) [
+                'methodCount' => count($info->methods),
+                'coveredMethodCount' => 0,
+                'statementCount' => 0,
+                'coveredStatementCount' => 0,
+                'conditionalCount' => 0,
+                'coveredConditionalCount' => 0,
+                'elementCount' => null,
+                'coveredElementCount' => null,
+        ];
 
-		foreach ($info->methods as $name => $methodInfo) {
-			[$lineCount, $coveredLineCount] = $this->analyzeMethod($methodInfo, $coverageData);
+        foreach ($info->methods as $name => $methodInfo) {
+            [$lineCount, $coveredLineCount] = $this->analyzeMethod($methodInfo, $coverageData);
 
-			$stats->statementCount += $lineCount;
+            $stats->statementCount += $lineCount;
 
-			if ($coverageData !== null) {
-				$stats->coveredMethodCount += $lineCount === $coveredLineCount ? 1 : 0;
-				$stats->coveredStatementCount += $coveredLineCount;
-			}
-		}
+            if ($coverageData !== null) {
+                $stats->coveredMethodCount += $lineCount === $coveredLineCount ? 1 : 0;
+                $stats->coveredStatementCount += $coveredLineCount;
+            }
+        }
 
-		$stats->elementCount = $stats->methodCount + $stats->statementCount;
-		$stats->coveredElementCount = $stats->coveredMethodCount + $stats->coveredStatementCount;
+        $stats->elementCount = $stats->methodCount + $stats->statementCount;
+        $stats->coveredElementCount = $stats->coveredMethodCount + $stats->coveredStatementCount;
 
-		return $stats;
-	}
+        return $stats;
+    }
+    
+    private function calculateStructuralMetrics(array $structuralLines, ?array $coverageData = null): \stdClass
+    {
+        $stats = (object) [
+                'statementCount' => 0,
+                'coveredStatementCount' => 0,
+                'elementCount' => null,
+                'coveredElementCount' => null,
+                'coveredLines' => [],
+        ];
+
+        if ($coverageData === null) { // Never loaded file
+            return $stats;
+        }
+
+        foreach ($structuralLines as $line => $val) {
+            if (isset($coverageData[$line]) && $coverageData[$line] !== self::LineDead) {
+                $stats->statementCount++;
+                if ($coverageData[$line] > 0) {
+                    $stats->coveredLines[] = $line;
+                    $stats->coveredStatementCount++;
+                }
+            }
+        }
+
+        $stats->elementCount = $stats->statementCount;
+        $stats->coveredElementCount = $stats->coveredStatementCount;
+        
+        return $stats;
+    }
 
 
-	private static function analyzeMethod(\stdClass $info, ?array $coverageData = null): array
-	{
-		$count = 0;
-		$coveredCount = 0;
+    private static function analyzeMethod(\stdClass $info, ?array $coverageData = null): array
+    {
+        $count = 0;
+        $coveredCount = 0;
 
-		if ($coverageData === null) { // Never loaded file
-			$count = max(1, $info->end - $info->start - 2);
-		} else {
-			for ($i = $info->start; $i <= $info->end; $i++) {
-				if (isset($coverageData[$i]) && $coverageData[$i] !== self::LineDead) {
-					$count++;
-					if ($coverageData[$i] > 0) {
-						$coveredCount++;
-					}
-				}
-			}
-		}
+        if ($coverageData === null) { // Never loaded file
+            $count = max(1, $info->end - $info->start - 2);
+        } else {
+            for ($i = $info->start; $i <= $info->end; $i++) {
+                if (isset($coverageData[$i]) && $coverageData[$i] !== self::LineDead) {
+                    $count++;
+                    if ($coverageData[$i] > 0) {
+                        $coveredCount++;
+                    }
+                }
+            }
+        }
 
-		return [$count, $coveredCount];
-	}
-
-
-	private static function appendMetrics(\stdClass $summary, \stdClass $add): void
-	{
-		foreach ($add as $name => $value) {
-			$summary->{$name} += $value;
-		}
-	}
+        return [$count, $coveredCount];
+    }
 
 
-	private static function setMetricAttributes(DOMElement $element, \stdClass $metrics): void
-	{
-		foreach ($metrics as $name => $value) {
-			$element->setAttribute(self::$metricAttributesMap[$name], (string) $value);
-		}
-	}
+    private static function appendMetrics(\stdClass $summary, \stdClass $add): void
+    {
+        foreach ($add as $name => $value) {
+            $summary->{$name} += $value;
+        }
+    }
+
+
+    private static function setMetricAttributes(DOMElement $element, \stdClass $metrics): void
+    {
+        foreach ($metrics as $name => $value) {
+            $element->setAttribute(self::$metricAttributesMap[$name], (string) $value);
+        }
+    }
 }
