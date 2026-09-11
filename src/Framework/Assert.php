62103c534d80bb323a2716b876d3e9b409822280
David Grudl <david@grudl.com>
2026-09-11 18:21:04 +0200

    uses dresscode
    DressCode replaces Nette Coding Standard in CI, which now runs on PHP 8.4.
    It is installed from dev-master, as it has no tagged release yet.
    .gitattributes normalizes line endings of all text files (text=auto).

--- src/Framework/Assert.php
+++ src/Framework/Assert.php
@@ -282,7 +282,7 @@ class Assert
 				self::fail(self::describe("%1 should be $type", $description), $value);
 			}
 		} elseif (in_array($type, ['array', 'bool', 'callable', 'float',
-			'int', 'integer', 'null', 'object', 'resource', 'scalar', 'string', ], strict: true)
+			'int', 'integer', 'null', 'object', 'resource', 'scalar', 'string'], strict: true)
 		) {
 			if (!('is_' . ($type === 'integer' ? 'int' : $type))($value)) {
 				self::fail(self::describe(get_debug_type($value) . " should be $type", $description));
